6a8062a2c23143ec1ff35b53701ee06a0ab1e1e2
JesusFranC2 <94206855+JesusFranC@users.noreply.github.com>
2026-07-09 01:11:41 -0700

    Git ignore, working on db access
    I am adding a git ignore file, as well as implementing DF to PostgreSQL DB code

--- Submissions/Project1/SourceCode/main.py
+++ Submissions/Project1/SourceCode/main.py
@@ -1,6 +1,7 @@
 from meteoWeatherApi import OpenMeteoClient
 from cleanweather import flatten_weather, profile_dataframe, clean_dataframe
 import pandas as pd
+import requests
 
 cities = [("Long Beach", 33.7767, -118.1892),
           ("Monterey", 36.6002, -121.8947),
@@ -11,22 +12,38 @@ def main():
 
     frames = []
     for city, lat, lon in cities:
-        weather = client.fetch(
-            lat,
-            lon,
-            "2020-01-01",
-            "2026-12-31"
-        )
-        # Flatten
-        frames.append(flatten_weather(city, weather))
+        # Catching errors if present
+        try:
+            weather = client.fetch(
+                lat,
+                lon,
+                "2020-01-01",
+                "2026-07-01"
+            )
+            # Flatten
+            frames.append(flatten_weather(city, weather))
+        
+        except requests.exceptions.HTTPError as e:
+            print(f"HTTP error while fetching weather for {city}:")
+            print(e)
+            continue
+
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed while fetching weather for {city}:")
+            print(e)
+            continue
+    
+    # Early exit if the api failed
+    if not frames:
+        print("No weather data was successfully retrieved.")
+        return
     
     # Gather all frames, profile them (output to command line, then cleans odd or bad data)
     df = pd.concat(frames, ignore_index=True)
     profile_dataframe(df)
-    df = clean_dataframe(df)
+    df = clean_dataframe(df) # Note: this also prints the head
 
-    # Get the head
-    print(df.head())
+    # Store to DB
 
 if __name__ == "__main__":
     main()
